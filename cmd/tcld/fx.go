f3a2f6dbdebc9da9f42d6085bd6b7ba00f774c93
Abhinav Nekkanti <10552725+anekkanti@users.noreply.github.com>
2022-08-30 06:44:16 +0000

    return a non-zero exit code on errors

--- cmd/tcld/fx.go
+++ cmd/tcld/fx.go
@@ -34,7 +34,10 @@ func fxOptions() fx.Option {
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "%s\n", err.Error())
 			}
-			return shutdowner.Shutdown()
+			if shutdownErr := shutdowner.Shutdown(); shutdownErr != nil {
+				fmt.Fprintf(os.Stderr, "failed to shutdown app: %s\n", shutdownErr.Error())
+			}
+			return err
 		}),
 		fx.NopLogger,
 	)
