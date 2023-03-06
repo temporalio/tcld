a29e3315d056ddcf39fe05de679b584493746e80
matt@temporal.io <matthkim@gmail.com>
2023-03-06 14:12:11 -0800

    add support for request signatures

--- app/flags.go
+++ app/flags.go
@@ -14,6 +14,10 @@ const (
 	NamespaceFlagName       = "namespace"
 	RequestIDFlagName       = "request-id"
 	ResourceVersionFlagName = "resource-version"
+	ServiceNameFlagName     = "service-name"
+	APIKeyIDFlagName        = "api-key-id"
+	APISecretKeyFlagName    = "api-secret-key"
+	EnableDebugLogsFlagName = "enable-debug-logs"
 )
 
 var (
@@ -56,5 +60,27 @@ var (
 		Name:    ResourceVersionFlagName,
 		Usage:   "The resource-version (etag) to update from, if not set the cli will use the latest (optional)",
 		Aliases: []string{"v"},
+	}
+	ServiceNameFlag = &cli.StringFlag{
+		Name:    ServiceNameFlagName,
+		Usage:   "The service name of the server",
+		Value:   "saas-api",
+		Hidden:  true,
+		EnvVars: []string{"TEMPORAL_CLOUD_SERVICE_NAME"},
+	}
+	APIKeyIDFlag = &cli.StringFlag{
+		Name:    APIKeyIDFlagName,
+		Usage:   "The API Key ID used for authentication",
+		EnvVars: []string{"TEMPORAL_CLOUD_API_KEY_ID"},
+	}
+	APISecretKeyFlag = &cli.StringFlag{
+		Name:    APISecretKeyFlagName,
+		Usage:   "The API Secret Key used for authentication",
+		EnvVars: []string{"TEMPORAL_CLOUD_API_SECRET_KEY"},
+	}
+	EnableDebugLogsFlag = &cli.BoolFlag{
+		Name:    EnableDebugLogsFlagName,
+		Usage:   "A flag to enable debug logs",
+		EnvVars: []string{"TEMPORAL_CLOUD_ENABLE_DEBUG_LOGS"},
 	}
 )
