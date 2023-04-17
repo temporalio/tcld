5adca29b54437368d30bc8a0685eeb21f0b3dd6d
Abhinav Nekkanti <10552725+anekkanti@users.noreply.github.com>
2023-04-17 13:18:06 -0700

    fix

--- services/loginservice.go
+++ services/loginservice.go
@@ -50,10 +50,3 @@ func (c *loginService) DeleteConfigFile(configPath string) error {
 	}
 	return nil
 }
-
-func (c *loginService) DeleteConfigFile(configPath string) error {
-	if _, err := os.Stat(configPath); err == nil {
-		return os.RemoveAll(configPath)
-	}
-	return nil
-}
