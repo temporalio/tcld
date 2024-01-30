89798f1158f48d2b29ce2ebd753ae68da1101c78
Travis Szucs <1060873+tminusplus@users.noreply.github.com>
2024-01-30 09:53:58 -0800

    Fix service account name

--- app/serviceaccount.go
+++ app/serviceaccount.go
@@ -230,7 +230,7 @@ func NewServiceAccountCommand(getServiceAccountClientFn GetServiceAccountClientF
 						}
 
 						spec := &auth.ServiceAccountSpec{
-							Name: serviceAccountIDFlagName,
+							Name: ctx.String(serviceAccountIDFlagName),
 							Access: &identity.Access{
 								AccountAccess: &identity.AccountAccess{
 									Role: ctx.String(accountRoleFlagName),
