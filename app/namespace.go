10df7ec2c220b00e8fa2604d29c56fc16af61954
Shakeel Rao <shakeelrao79@gmail.com>
2024-04-18 17:05:35 -0400

    rm comment

--- app/namespace.go
+++ app/namespace.go
@@ -1974,7 +1974,6 @@ func compareCodecSpec(existing, replacement *namespace.CodecServerPropertySpec) 
 	return diff.Diff(string(existingBytes), string(replacementBytes)), nil
 }
 
-// Determines if an `AuthMethod` update will fail existing client connections.
 func disruptiveChange(old namespace.AuthMethod, new namespace.AuthMethod) bool {
 	return old != namespace.AUTH_METHOD_RESTRICTED && new != namespace.AUTH_METHOD_API_KEY_OR_MTLS
 }
