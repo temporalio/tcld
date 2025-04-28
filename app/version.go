1a994a55eac1044c530940d807c18dc87678bd21
shivam-ajmera <shivamajmera@hotmail.com>
2025-04-28 12:08:44 -0400

    s

--- app/version.go
+++ app/version.go
@@ -19,7 +19,7 @@ const (
 	// MinSupportedVersion is the minimum tcld version supported by our APIs.
 	// This string must be updated when we deprecate older versions, but should be
 	// done carefully as this will likely break user's current usage of tcld.
-	MinSupportedVersion = "v0.21.0"
+	MinSupportedVersion = "v0.22.0"
 
 	// DefaultVersionString is the version which is sent over if no version was available.
 	// This can happen if a user builds the latest main branch, as the version string provided
