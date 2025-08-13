04a03bff51a971c075d2b7d23e2ba48b3c279e9a
Gregory Mankes <gregory.mankes@temporal.io>
2025-08-13 12:02:05 -0400

    CLDIAM-307 - add default user agent to dial opts (#469)
    * add default user agent to dial opts
    
    (will fill in the actual user agent when we know what to put here)
    
    * add build info as user agent at initialization
    
    doing it at init so that we don't waste cycles on every connection being
    opened

--- app/connection.go
+++ app/connection.go
@@ -26,11 +26,20 @@ const (
 	TemporalCloudAPIVersionHeader = "temporal-cloud-api-version"
 	LegacyTemporalCloudAPIVersion = "2025-07-09-00"
 	TemporalCloudAPIVersion       = "v0.5.1"
+	userAgentTemplate             = "tcld/%s"
 )
 
 var (
 	TemporalCloudAPIMethodRegex = regexp.MustCompile(`^\/temporal\.api\.cloud\.cloudservice\.v1\.CloudService\/[^\/]*$`)
+
+	// UserAgent is set on package initialization.
+	UserAgent string
 )
+
+func init() {
+	buildInfo := NewBuildInfo()
+	UserAgent = fmt.Sprintf(userAgentTemplate, buildInfo.Version)
+}
 
 func GetServerConnection(c *cli.Context, opts ...grpc.DialOption) (context.Context, *grpc.ClientConn, error) {
 	addr, err := url.Parse(c.String(ServerFlagName))
@@ -88,6 +97,7 @@ func defaultDialOptions(c *cli.Context, addr *url.URL) ([]grpc.DialOption, error
 			unaryVersionInterceptor,
 			grpcretry.UnaryClientInterceptor(retryOpts...),
 		),
+		grpc.WithUserAgent(UserAgent),
 	}
 
 	creds, err := newRPCCredential(c)
